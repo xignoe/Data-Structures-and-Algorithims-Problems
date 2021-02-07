605a61292cffdc9e4798c02187de0498af1b4c11
xignoe <51763652+xignoe@users.noreply.github.com>
2021-02-06 20:12:31 -0500

    Update Linked List Java.java

--- Linked List Java.java
+++ Linked List Java.java
@@ -5,4 +5,51 @@ public class Node {
         this.data = data;
     }
 
+
+public class LinkedList {
+    Node head;
+
+
+    public void append(int data) {
+        if (head == null) {
+            head = new Node(data);
+            return;
+        }
+
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+
+        current.next = new Node(data);
+    }
+
+    public void prepend(int data) {
+        Node newHead = new Node(data);
+        newHead.next = head;
+        head = newHead;
+    }
+
+    public void deleteWithValue(int data) {
+        if (head == null) return;
+
+        if (head.data == data) {
+            head = head.next;
+            return;
+        }
+
+        Node current = head;
+        while (current.next != null) {
+            if (current.next.data == data) {
+                current.next = current.next.next;
+                return;
+            }
+
+            current = current.next;
+        }
+    }
+
+
+
 }
+        
