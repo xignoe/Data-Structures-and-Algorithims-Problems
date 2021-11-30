4b628a6d7237a0a2346acc6be128a36e6b78328e
xignoe <51763652+xignoe@users.noreply.github.com>
2021-11-29 23:14:47 -0500

    HackerRank problems

---
+++ Python/mini-max sum.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'miniMaxSum' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def miniMaxSum(arr):
+    # Write your code here
+    maxSum = sum(arr) - min(arr)
+    minSum = sum(arr) - max(arr)
+        
+    print(minSum, maxSum)
+
+if __name__ == '__main__':
+
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum(arr)
