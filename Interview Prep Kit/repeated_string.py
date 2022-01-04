d3110e8fccd159330ed6e003387ba9409361051f
xignoe <51763652+xignoe@users.noreply.github.com>
2022-01-04 01:47:27 -0500

    update

---
+++ Interview Prep Kit/repeated_string.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'repeatedString' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. STRING s
+#  2. LONG_INTEGER n
+#
+
+def repeatedString(s, n):
+    quotient = n // len(s)
+    remainder = n % len(s)
+    totalQuo = s.count('a') * quotient
+    totalRem = s[0:remainder].count('a')
+    return totalQuo + totalRem
+    
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    n = int(input().strip())
+
+    result = repeatedString(s, n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
