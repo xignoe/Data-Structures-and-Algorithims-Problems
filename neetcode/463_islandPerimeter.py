74a0b1aa610ed90468cbc3245abfb03458fc9a7c
xignoe <51763652+xignoe@users.noreply.github.com>
2023-11-07 23:43:02 -0500

    upload leetcode problem

---
+++ neetcode/463_islandPerimeter.py
@@ -0,0 +1,17 @@
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        rows = len(grid)
+        columns = len(grid[0])
+
+        perimeter = 0
+
+        for i in range(rows):
+            for j in range(columns):
+                if grid[i][j] == 1:
+                    perimeter += 4
+                    if i > 0 and grid[i-1][j] == 1:
+                        perimeter -= 2
+                    if j > 0 and grid[i][j-1] == 1:
+                        perimeter -= 2
+        
+        return perimeter
